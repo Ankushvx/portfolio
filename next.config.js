c14d54992ce702eaa711f9b9a5b5805ee3f16e13
ANKUSH Verma <ankushvx@gmail.com>
2025-10-13 12:06:08 +0530

    `Update .gitignore and next.config.js to include new directories`

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { 
-  output: 'export',  
-  basePath: '/portfolio_web',        // 👈 Repo name
-  images: { unoptimized: true }, // 👈 Disable Next.js image optimization for static
-  // reactStrictMode: true,  
-  // swcMinify: true,
+const nextConfig = {    
+  reactStrictMode: true,  
+  swcMinify: true,
 }
 
 module.exports = nextConfig
